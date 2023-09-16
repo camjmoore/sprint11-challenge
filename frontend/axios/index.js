05dab0f945cc5eba84c898f39b6736ea78f890a6
octanym <camjmoore@outlook.com>
2023-09-16 08:57:53 -0400

    implemented axios with auth

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,10 @@
-// ✨ implement axiosWithAuth
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+    baseUrl: 'https://localhost:5001/api',
+    headers: { authorization: token },
+  });
+};
