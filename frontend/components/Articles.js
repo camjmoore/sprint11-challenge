4f5c3998b573b3ceb2229b52716b15fd4d36d1d6
octanym <camjmoore@outlook.com>
2023-09-17 23:06:05 -0400

    fetch articles, pass prop functions to onClicks and conditionally navigate to login page

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -14,6 +14,9 @@ export default function Articles(props) {
 
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
+  if (!localStorage.getItem('token')) {
+    return <Navigate to="/login" replace={true} />;
+  }
 
   useEffect(() => {
     // ✨ grab the articles here, on first render only
@@ -25,9 +28,9 @@ export default function Articles(props) {
     // and use the articles prop to generate articles
     <div className="articles">
       <h2>Articles</h2>
-      {![].length
+      {!articles.length
         ? 'No articles yet'
-        : [].map((art) => {
+        : articles.map((art) => {
             return (
               <div className="article" key={art.article_id}>
                 <div>
@@ -36,10 +39,13 @@ export default function Articles(props) {
                   <p>Topic: {art.topic}</p>
                 </div>
                 <div>
-                  <button disabled={true} onClick={Function.prototype}>
+                  <button
+                    disabled={true}
+                    onClick={setCurrentArticleId(currentArticleId)}
+                  >
                     Edit
                   </button>
-                  <button disabled={true} onClick={Function.prototype}>
+                  <button disabled={true} onClick={deleteArticle()}>
                     Delete
                   </button>
                 </div>
