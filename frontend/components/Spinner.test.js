85ccc2d277dcfc201f75575afdd2dfac18d61ada
camjmoore <cjmooredev@gmail.com>
2023-10-25 13:06:12 -0400

    tested values of spinner component data when boolean prop changes

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,26 @@
-// Import the Spinner component into this file and test
-// that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import Spinner from "./Spinner";
+import { render, screen } from "@testing-library/react";
+
+test("renders message when on prop is true", () => {
+  render(<Spinner on={true} />);
+  const value = screen.queryByText(/Please wait.../i);
+  expect(value).not.toBeNull;
+});
+
+test("renders spinner when on prop is true", () => {
+  const spinner = screen.queryByTestId("spinner");
+
+  render(<Spinner on={true} />);
+  expect(spinner).not.toBeNull;
+});
+
+test("renders null when on prop is false", () => {
+  const spinner = screen.queryByTestId("spinner");
+
+  const { rerender } = render(<Spinner on={true} />);
+  expect(spinner).not.toBeNull;
+
+  rerender(<Spinner on={false} />);
+  expect(spinner).toBeNull;
+});
